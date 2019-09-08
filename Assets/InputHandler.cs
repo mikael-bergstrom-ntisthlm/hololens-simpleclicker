7aa633398a4c6c0d282d6e2bf6a77d26fa39abc8
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2019-09-08 13:15:32 +0200

    Initial commit

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+
+public class InputHandler : MonoBehaviour, IInputClickHandler
+{
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        print("heeey");
+    }
+
+    void Start () {
+
+        InputManager.Instance.PushModalInputHandler(gameObject);
+        InputManager.Instance.AddGlobalListener()
+
+	}
+
+}
---
+++ Assets/Scripts/RotateOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class RotateOnClick : MonoBehaviour, IInputClickHandler {
+
+    bool isRotating = false;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        isRotating = !isRotating;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isRotating)
+        {
+            transform.Rotate(Vector3.up, 45 * Time.deltaTime);
+        }
+	}
+}
